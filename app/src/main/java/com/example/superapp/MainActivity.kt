14ca18f7dd2573d639fe2ecc0b431a9ab58823b1
Redsquare <91898346+RedsquareM@users.noreply.github.com>
2022-12-25 12:43:21 -0500

    navigation from one screen to another
    added the code to navigate from one screen to another using bottom navigation bar

--- app/src/main/java/com/example/superapp/MainActivity.kt
+++ app/src/main/java/com/example/superapp/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.superapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    //global variable reference
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favoriteRecipesFragment,
+                R.id.foodJokeFragment
+            )
+        )
+        //pereberatsya s ekrana na ekran
+        //navigation access view from activity
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+
     }
+    //menyat' odin ekran na drugoi
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+
 }
+
+
